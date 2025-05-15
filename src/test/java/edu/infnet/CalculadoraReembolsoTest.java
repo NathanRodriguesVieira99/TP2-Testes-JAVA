32971b0721680b5a2924d9e462a9619a1e9e790e
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-15 16:10:24 -0300

    feat(tests): enhance CalculadoraReembolsoTest to include patient and health plan parameters in reimbursement calculations

--- src/test/java/edu/infnet/CalculadoraReembolsoTest.java
+++ src/test/java/edu/infnet/CalculadoraReembolsoTest.java
@@ -3,11 +3,17 @@ package edu.infnet;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 
+import edu.infnet.interfaces.PlanoSaude;
+import edu.infnet.stubs.PlanoSaude50Stub;
+import edu.infnet.stubs.PlanoSaude80Stub;
+
 /**
  *
  * @author nathan.vieira
  */
 public class CalculadoraReembolsoTest {
+
+    private final Paciente pacientDummy = new Paciente();
 
     @Test
     public void ShouldCalculateReimbursementWith70Percent() {
@@ -22,7 +28,8 @@ public class CalculadoraReembolsoTest {
         // Act
 
         // chamo o método calculateReimbursement passando os valores
-        double reimbursementValue = calculadora.calculateReimbursement(consultationValue, percentualReimbursement);
+        double reimbursementValue = calculadora.calculateReimbursement(consultationValue, percentualReimbursement,
+                pacientDummy, null);
 
         // Assert
 
@@ -44,7 +51,8 @@ public class CalculadoraReembolsoTest {
         // Act
 
         // chamo o método calculateReimbursement passando os valores
-        double reimbursementValue = calculadora.calculateReimbursement(consultationValue, percentualReimbursement);
+        double reimbursementValue = calculadora.calculateReimbursement(consultationValue, percentualReimbursement,
+                pacientDummy, null);
 
         // Assert
 
@@ -65,13 +73,48 @@ public class CalculadoraReembolsoTest {
         // Act
 
         // chamo o método calculateReimbursement passando os valores
-        double reimbursementValue = calculadora.calculateReimbursement(consultationValue, percentualReimbursement);
+        double reimbursementValue = calculadora.calculateReimbursement(consultationValue, percentualReimbursement,
+                pacientDummy, null);
 
         // Assert
 
         // verifica se o valor do reembolso calculado é igual a 100 (100% )
         assertEquals("O reembolso deve ser igual ao valor integral da consulta (100%)",
                 consultationValue, reimbursementValue, 0.001);
+    }
+
+    @Test
+    public void ShouldCalculateWith50PercentPlan() {
+        // Arrange
+        CalculadoraReembolso calculadora = new CalculadoraReembolso();
+
+        double consultationValue = 200.0;
+        double percentualReimbursement = 50.0;
+        PlanoSaude plano50 = new PlanoSaude50Stub();
+
+        // Act
+        double reimbursement = calculadora.calculateReimbursement(consultationValue, percentualReimbursement,
+                pacientDummy, plano50);
+
+        // Assert
+        assertEquals(100, reimbursement, 0.001);
+    }
+
+    @Test
+    public void ShouldCalculateWith80PercentPlan() {
+        // Arrange
+        CalculadoraReembolso calculadora = new CalculadoraReembolso();
+
+        double consultationValue = 200.0;
+        double percentualReimbursement = 80.0;
+        PlanoSaude plano80 = new PlanoSaude80Stub();
+
+        // Act
+        double reimbursement = calculadora.calculateReimbursement(consultationValue, percentualReimbursement,
+                pacientDummy, plano80);
+
+        // Assert
+        assertEquals(100, reimbursement, 0.001);
     }
 
 }
