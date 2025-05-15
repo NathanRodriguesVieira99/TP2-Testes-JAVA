4e1a2e9090b9decdde27bb6c5bb4caaf00eef5a3
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-05-15 11:32:15 -0300

    test(CalculadoraReembolsoTest): create ShouldCalculateReimbursementWith0Percent and ShouldCalculateReimbursementWith100Percent test suits

---
+++ src/test/java/edu/infnet/CalculadoraReembolsoTest.java
@@ -0,0 +1,77 @@
+package edu.infnet;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+/**
+ *
+ * @author nathan.vieira
+ */
+public class CalculadoraReembolsoTest {
+
+    @Test
+    public void ShouldCalculateReimbursementWith70Percent() {
+        // Arrange
+
+        // aqui chamo a classe CalculeteReimbursement
+        CalculadoraReembolsoTest calculadora = new CalculadoraReembolsoTest();
+        // declaro os valores
+        double consultationValue = 200.0;
+        double percentualReimbursement = 70.0;
+
+        // Act
+
+        // chamo o método calculateReimbursement passando os valores
+        double reimbursementValue = calculadora.calculateReimbursement(consultationValue, percentualReimbursement);
+
+        // Assert
+
+        // verifica se o valor do reembolso calculado é igual a 140.0 (70% de 200)
+        assertEquals("O reembolso de 70% de R$200 deve ser R$140", 140.0, reimbursementValue, 0.001);
+
+    }
+
+    @Test
+    public void ShouldCalculateReimbursementWith0Percent() {
+        // Arrange
+
+        // aqui chamo a classe CalculeteReimbursement
+        CalculadoraReembolsoTest calculadora = new CalculadoraReembolsoTest();
+        // declaro os valores
+        double consultationValue = 200.0;
+        double percentualReimbursement = 0;
+
+        // Act
+
+        // chamo o método calculateReimbursement passando os valores
+        double reimbursementValue = calculadora.calculateReimbursement(consultationValue, percentualReimbursement);
+
+        // Assert
+
+        // verifica se o valor do reembolso calculado é igual a 0 (0% )
+        assertEquals("O reembolso deve ser 0 quando for 0% de reembolso ", 0, reimbursementValue, 0.001);
+    }
+
+    @Test
+    public void ShouldCalculateReimbursementWith100Percent() {
+        // Arrange
+
+        // aqui chamo a classe CalculeteReimbursement
+        CalculadoraReembolsoTest calculadora = new CalculadoraReembolsoTest();
+        // declaro os valores
+        double consultationValue = 200.0;
+        double percentualReimbursement = 100;
+
+        // Act
+
+        // chamo o método calculateReimbursement passando os valores
+        double reimbursementValue = calculadora.calculateReimbursement(consultationValue, percentualReimbursement);
+
+        // Assert
+
+        // verifica se o valor do reembolso calculado é igual a 100 (100% )
+        assertEquals("O reembolso deve ser igual ao valor integral da consulta (100%)",
+                consultationValue, reimbursementValue, 0.001);
+    }
+
+}
