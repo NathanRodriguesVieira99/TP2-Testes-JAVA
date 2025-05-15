0e91252a42a5bb959d68b343c4f8fe4389fc1f54
Nathan Rodrigues Vieira <nathanrodriguesvieira99@gmail.com>
2025-05-15 11:35:16 -0300

    fix(CalculeteReimbursement): change class name

--- src/main/java/edu/infnet/CalculadoraReembolso.java
+++ src/main/java/edu/infnet/CalculadoraReembolso.java
@@ -5,7 +5,7 @@ package edu.infnet;
  *
  * @author nathan.vieira
  */
-public class CalculeteReimbursement {
+public class CalculadoraReembolso {
 
     public double calculateReimbursement(double consultationValue, double percentualReimbursement) {
         return consultationValue * (percentualReimbursement / 100);
