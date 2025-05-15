df72149b354ad6ac2653e4826776b974ef136729
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-15 16:10:06 -0300

    feat(CalculadoraReembolso): update calculateReimbursement method to include Paciente and PlanoSaude parameters

--- src/main/java/edu/infnet/CalculadoraReembolso.java
+++ src/main/java/edu/infnet/CalculadoraReembolso.java
@@ -1,13 +1,16 @@
 
 package edu.infnet;
 
+import edu.infnet.interfaces.PlanoSaude;
+
 /**
  *
  * @author nathan.vieira
  */
 public class CalculadoraReembolso {
 
-    public double calculateReimbursement(double consultationValue, double percentualReimbursement) {
+    public double calculateReimbursement(double consultationValue, double percentualReimbursement, Paciente paciente,
+            PlanoSaude plano) {
         return consultationValue * (percentualReimbursement / 100);
 
     }
