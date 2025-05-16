bfe15d99d23c5a2e1df05a9ba09f299033d1df4c
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-16 13:59:38 -0300

    feat(CalculadoraReembolso): implement reimbursement calculation with authorization and auditing

--- src/main/java/edu/infnet/CalculadoraReembolso.java
+++ src/main/java/edu/infnet/CalculadoraReembolso.java
@@ -1,6 +1,9 @@
 
 package edu.infnet;
 
+import edu.infnet.Mocks.AutorizadorReembolso;
+import edu.infnet.exceptions.ReembolsoNaoAutorizadoException;
+import edu.infnet.interfaces.Auditoria;
 import edu.infnet.interfaces.PlanoSaude;
 
 /**
@@ -9,10 +12,28 @@ import edu.infnet.interfaces.PlanoSaude;
  */
 public class CalculadoraReembolso {
 
-    public double calculateReimbursement(double consultationValue, double percentualReimbursement, Paciente paciente,
-            PlanoSaude plano) {
-        return consultationValue * (percentualReimbursement / 100);
+    private final Auditoria auditoria;
+    private final AutorizadorReembolso autorizadorReembolso;
+    private static final double reembolsoLimite = 150.0;
 
+    public CalculadoraReembolso(Auditoria auditoria, AutorizadorReembolso autorizadorReembolso) {
+        this.auditoria = auditoria;
+        this.autorizadorReembolso = autorizadorReembolso;
+    }
+
+    public double calculateReimbursement(double consultationValue, double percentualReimbursement, Paciente paciente,
+            PlanoSaude plano) throws ReembolsoNaoAutorizadoException {
+        Consulta consulta = new Consulta(paciente, consultationValue, java.time.LocalDateTime.now(), plano);
+
+        if (!autorizadorReembolso.autorizeReimbursement(consulta, paciente)) {
+            throw new ReembolsoNaoAutorizadoException("Reembolso não autorizado");
+        }
+
+        auditoria.registConsultation(consulta);
+
+        double calculoValor = consultationValue * (percentualReimbursement / 100);
+
+        return Math.min(calculoValor, reembolsoLimite);
     }
 
 }
