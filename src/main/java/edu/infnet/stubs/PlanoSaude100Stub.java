840451f03492de2312530ef681fcf1914bf682cb
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-16 13:58:10 -0300

    feat(stubs): add PlanoSaude100Stub

---
+++ src/main/java/edu/infnet/stubs/PlanoSaude100Stub.java
@@ -0,0 +1,15 @@
+
+package edu.infnet.stubs;
+
+import edu.infnet.interfaces.PlanoSaude;
+
+/**
+ *
+ * @author hellb
+ */
+public class PlanoSaude100Stub implements PlanoSaude {
+    @Override
+    public double getPercentualCoverage() {
+        return 100.0;
+    }
+}
