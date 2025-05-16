2fab50d670950dc1ed078e7a9c5c7bf9f1641bed
Nathan Rodrigues Vieira <116213876+NathanRodriguesVieira99@users.noreply.github.com>
2025-05-16 13:58:56 -0300

    feat(exceptions): add ReembolsoNaoAutorizadoException

---
+++ src/main/java/edu/infnet/exceptions/ReembolsoNaoAutorizadoException.java
@@ -0,0 +1,12 @@
+
+package edu.infnet.exceptions;
+
+/**
+ *
+ * @author hellb
+ */
+public class ReembolsoNaoAutorizadoException extends Exception {
+    public ReembolsoNaoAutorizadoException(String message) {
+        super(message);
+    }
+}
